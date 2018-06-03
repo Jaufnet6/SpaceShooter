9a269cd76298df2ef0dc3ff7d296fe902cf5d9d2
JulienReichenbach <julien.reichenbach@students.hevs.ch>
2018-06-03 18:11:03 +0200

    Rework on changeColor method (still not working)

--- changeColor.js
+++ changeColor.js
@@ -3,40 +3,12 @@ function changeColor(a){
     alert("new Color: "+ newColor);
     getLocalStorageColor(newColor);
     localStorage.setItem("color", value);
-
 }
 
-
 function getLocalStorageColor(newColor){
-
     var oldColor = localStorage.getItem("color");
     alert("OldColor:" + oldColor);
     var cssLink = $('link[href*=oldColor]');
 
-    switch (newColor) {
-
-        case "styles/backgroundGreen.css" :
-            // change les couleur si vert
-            cssLink.replaceWith('<link href="styles/backgroundGreen.css" type="text/css" rel="stylesheet">');
-            break;
-
-
-        case  "styles/backgroundGrey.css" :
-            //change les couleurs si gris
-
-            cssLink.replaceWith('<link href="styles/backgroundGrey.css" type="text/css" rel="stylesheet">');
-
-            break;
-
-        case "styles/backgroundBlue.css" :
-            // change couleur bleu
-            cssLink.replaceWith('<link href="styles/backgroundBlue.css" type="text/css" rel="stylesheet">');
-            break;
-        default:
-            cssLink.replaceWith('<link href="styles/backgroundBlue.css" type="text/css" rel="stylesheet">');
-            break;
-
-    }
-
-
+    cssLink.replaceWith('<link href=newColor type="text/css" rel="stylesheet">');
 }
