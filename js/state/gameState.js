228446b93fbe2e7c61ff9dcb4a6418c88c80e3cd
aurelieglassey <aurelie.glassey@students.hevs.ch>
2018-06-03 10:37:01 +0200

    gamestate

--- js/state/gameState.js
+++ js/state/gameState.js
@@ -1,8 +1,5 @@
 var
-    /**
-     * Game objects
-     */
-
+ //Variables
     frames,
     spFrame,
     lvFrame,
@@ -25,6 +22,7 @@ var
 var GameState = State.extend({
 
 
+    //initialise le score et les vies pour demarrer le jeu
     init: function (game) {
         this._super(game);
         lives = 3;
@@ -32,40 +30,39 @@ var GameState = State.extend({
         score = 0;
         lvl = 1;
 
-
+        //Son de depart pour le jeu
         gameSound = document.getElementById("gameBackground");
         gameSound.loop = true;
         gameSound.volume = .25;
         gameSound.load();
         gameSound.play();
 
+        //Son pour shooter
         shoot = new SoundPool(10);
         shoot.init("shoot");
 
+        //son lors de l'explosion
         explosion = new SoundPool(20);
         explosion.init("explosion");
 
+        //son lorsque'un ennemie est tue
         killed = new SoundPool(10);
         killed.init("invaderKilled");
 
 
 
-
-        // score lives variables
-
-
-
-        // store canvas dimensions for later use
+        // dimensions du canevas
         canvasWidth = game.canvas.ctx.width;
         canvasHeight = game.canvas.ctx.height;
 
-        // set start settings
+        // Vitesses de base
         frames  = 0;
         spFrame = 0;
         lvFrame = 60;
 
         dir = 1;
-        // create the tank object
+
+        // Initalise le tank
         tank = {
             sprite: taSprite,
             x: (canvasWidth - taSprite.w) / 2,
@@ -74,19 +71,18 @@ var GameState = State.extend({
         };
 
 
-        // initatie bullet array
+        // initialise un tableau de boules
         bullets = [];
-        // create the cities object (and canvas)
+        // creation des villes
         cities = {
             canvas: null,
             ctx: 	null,
             y: tank.y - (30 + ciSprite.h),
             h: ciSprite.h,
-            /**
-             * Create canvas and game graphic context
-             */
+
+
+            //Creation du canevas
             init: function() {
-                // create canvas and grab 2d context
                 this.canvas = document.createElement("canvas");
                 this.canvas.width = canvasWidth;
                 this.canvas.height = canvasHeight;
@@ -97,17 +93,14 @@ var GameState = State.extend({
                         68 + 111*i, 0, ciSprite.w, ciSprite.h);
                 }
             },
-            /**
-             * Create damage effect on city-canvas
-             *
-             * @param  {number} x x-coordinate
-             * @param  {number} y y-coordinate
-             */
+
+            // Affectation des villes
+            // pour chaque position on va retirer des pixels
             generateDamage: function(x, y) {
                 // round x, y position
                 x = Math.floor(x/2) * 2;
                 y = Math.floor(y/2) * 2;
-                // draw dagame effect to canva
+                // ClearRect permet d'effacer des pixels
                 this.ctx.clearRect(x-2, y-2, 4, 4);
                 this.ctx.clearRect(x+2, y-4, 2, 4);
                 this.ctx.clearRect(x+4, y, 2, 2);
@@ -117,34 +110,34 @@ var GameState = State.extend({
                 this.ctx.clearRect(x-4, y-4, 2, 2);
                 this.ctx.clearRect(x-2, y-6, 2, 2);
             },
-            /**
-             * Check if pixel at (x, y) is opaque
-             *
-             * @param  {number} x x-coordinate
-             * @param  {number} y y-coordinate
-             * @return {bool}     boolean value if pixel opaque
-             */
+
+            // Controle si le pxel a deja ete touche
             hits: function(x, y) {
-                // transform y value to local coordinate system
+
                 y -= this.y;
-                // get imagedata and check if opaque
+                // Verifie si le pixel est opaque
                 var data = this.ctx.getImageData(x, y, 1, 1);
                 if (data.data[3] !== 0) {
+                    // Si opaque, genere des dommages sur la cite
                     this.generateDamage(x, y);
                     return true;
                 }
                 return false;
             }
         };
-        cities.init(); // initiate the cities
-        // create and populate alien array
+
+       //Initialise les villes
+        cities.init();
+
+        // Cree plusieurs aliens dans un tableau
         aliens = [];
         var rows = [1, 0, 0, 2, 2];
         for (var i = 0, len = rows.length; i < len; i++) {
             for (var j = 0; j < 10; j++) {
                 var a = rows[i];
-                // create right offseted alien and push to alien
-                // array
+
+                // Affect chaque sprite à l'alien
+                // Et les pousses un apres l'autre
                 aliens.push({
                     sprite: alSprite[a],
                     x: 30 + j*30 + [0, 4, 0][a],
@@ -156,43 +149,40 @@ var GameState = State.extend({
         }
     },
 
-
-
-
-
-
-    /**
-     * @override State.handleInputs
-     *
-     * @param  {InputHandeler} input keeps track of all pressed keys
-     */
+    // Gestion des touches clavier : barre d'espace, droit, gauche
     handleInputs: function(input) {
-        // only update tank orientation and velocity if it's visible
+
+        //Si le tank n'est pas visible
         if (!tank.visible) {
+            // et que la barre d'espace est pressee
             if (input.isPressed("spacebar")) {
-                // change state if game over
+               // Check si le jeu doit s'arreter
+                // arrete la musique et change d'état
                 if (gameOver) {
                     gameSound.pause();
                     game.nextState = States.END;
                     game.stateVars.score = this.score;
                     return;
                 }
+                // le tank est mis a visible
                 tank.visible = true;
             }
             return;
         }
-        // update tank position depending on pressed keys
-        if (input.isDown("left")) { // Left
+
+        // Direction du tank
+        if (input.isDown("left")) {
             tank.x -= 4;
         }
-        if (input.isDown("right")) { // Right
+        // Direction du tank
+        if (input.isDown("right")) {
             tank.x += 4;
         }
 
 
-        // append new bullet to the bullet array if spacebar is
-        // pressed
-        if (input.isPressed("spacebar")) { // Space
+        // Permet de creer une ball et la tire
+        // Quand on appuie sur la touche espace
+        if (input.isPressed("spacebar")) {
             bullets.push(new Bullet(tank.x + 10, tank.y, -8, 2, 6, "#fff"));
             shoot.get();
         }
@@ -201,34 +191,34 @@ var GameState = State.extend({
     },
 
 
-
-
-    /**
-     * Update the game logic
-     */
+    // Met à jour la logique du jeu
     update: function() {
-        // update the frame count
+
+        //Augmente les frames à chaque mise a jour
         frames++;
 
-        // keep the tank sprite inside of the canvas
+        // Bloque le tank a l'intérieur du canevas
         tank.x = Math.max(Math.min(tank.x, canvasWidth - (30 + taSprite.w)), 30);
 
-        // update all bullets position and checks
+        // met a jour toutes les balles
+        // et verifie la logique des balles
         for (var i = 0, nbBullet = bullets.length; i < nbBullet; i++) {
             var b = bullets[i];
             b.update();
 
-            // remove bullets outside of the canvas
+            // efface les balles
             if (b.y + b.height < 0 || b.y > canvasHeight) {
                 bullets.splice(i, 1);
                 i--;
                 nbBullet--;
                 continue;
             }
-            // check if bullet hits any city
+
+            //controle si les balles ont touche les cites
             var h2 = b.height * 0.5;
 
 
+            // Detruit les balles quand une cite est touchee
             if (cities.y < b.y+h2 && b.y+h2 < cities.y + cities.h) {
                 if (cities.hits(b.x, b.y+h2)) {
                     bullets.splice(i, 1);
@@ -238,39 +228,49 @@ var GameState = State.extend({
                 }
             }
 
+            //si le tank est visible
             if (tank.visible == true) {
-                //if tank get hit by bullet
+                // Si le tank est heurte par une balle
                 if (touche(b.x, b.y, b.width, b.height, tank.x, tank.y, taSprite.w, taSprite.h)) {
 
+                    //Efface la balle
                     bullets.splice(i, 1);
                     nbBullet--;
 
+                    //reduit le nombre de vie du joueur
                     lives--;
                     if (lives <= 0) {
                         tank.visible = false;
                         gameOver = true;
 
                     }
+
+                    //reinitalise la position du tank
                     tank.x = (canvasWidth - taSprite.w) / 2;
                     tank.y = canvasHeight - (30 + taSprite.h);
+                    //Appelle le son d'explosion
                     explosion.get();
 
+                    //A chaque fois que le tank est touche, il disparait
                     tank.visible = false;
 
                 }
             }
 
-            // check if bullet hit any aliens and check if the alien touch a city
+            // Controle si les balles touche une ville ou un alien
             for (var j = 0, nbAlien = aliens.length; j < nbAlien; j++) {
                 var a = aliens[j];
 
+                // Quand les aliens touchent physiquement une ville
+                // le jeu s'arrete
                 if (touche(ciSprite.x, cities.y, ciSprite.w, cities.h, a.x, a.y, a.w, a.h)){
                     gameOver = true;
                     tank.visible = false;
                     lvFrame = 0;
                 }
 
-
+                // Verifie quand une balle touche un alien
+                // l'alien disparait
                 if (touche(b.x, b.y, b.width, b.height, a.x, a.y, a.w, a.h)) {
                     killed.get();
                     aliens.splice(j, 1);
@@ -279,11 +279,14 @@ var GameState = State.extend({
                     bullets.splice(i, 1);
                     i--;
                     nbBullet--;
-                    score += 51-nbAlien;
-                    // increase the movement frequence of the aliens
-                    // when there are less of them
 
+                    //les points augmentent
+                    // selon les aliens restant
+                    // et selon le niveau de vague des aliens
+                    score += 51-nbAlien*lvl;
 
+                    // augmente la vitesse en fonction du nombre d'alien restant
+                    // dans le tableau actuel
                     switch (nbAlien) {
                         case 45: {
                             lvFrame = lvFrame - 1;
@@ -317,6 +320,7 @@ var GameState = State.extend({
                         }
                     }
 
+                    // Augmente la vitesse selon le niveau de vague du joueur
                     switch (lvl) {
                         case 1: {
                             lvFrame = 45;
@@ -352,36 +356,43 @@ var GameState = State.extend({
                 }
             }
         }
-        // makes the alien shoot in an random fashion
+
+
+       // Fais tirer les aliens sur le tank de maniere aleatoire
         if (Math.random() < 0.03 && aliens.length > 0) {
             var a = aliens[Math.round(Math.random() * (aliens.length - 1))];
-            // iterate through aliens and check collision to make
-            // sure only shoot from front line
+            //Verifie que la première ligne d'alien tire seulement
             for (var i = 0, len = aliens.length; i < len; i++) {
                 var b = aliens[i];
                 if (touche(a.x, a.y, a.w, 100, b.x, b.y, b.w, b.h)) {
                     a = b;
                 }
             }
-            // create and append new bullet
+            // creation de la balle
             bullets.push(new Bullet(a.x + a.w*0.5, a.y + a.h, 4, 2, 4, "#fff"));
         }
-        // update the aliens at the current movement frequence
+
+
+        // met a jour les aliens sur chaque frames
         if (frames % lvFrame === 0) {
             spFrame = (spFrame + 1) % 2;
             var _max = 0, _min = canvasWidth;
-            // iterate through aliens and update postition
+
+            //met à jour la position des aliens
             for (var i = 0, len = aliens.length; i < len; i++) {
                 var a = aliens[i];
                 a.x += 30 * dir;
-                // find min/max values of all aliens for direction
-                // change test
+
+                // trouve la valeur maximal a droite ou gauche
                 _max = Math.max(_max, a.x + a.w);
                 _min = Math.min(_min, a.x);
             }
-            // check if aliens should move down and change direction
+
+            // regare dans quel direction les aliens doivent se diriger
+            // et descendent
             if (_max > canvasWidth - 30 || _min < 30) {
-                // mirror direction and update position
+
+                //mettent a jour la position
                 dir *= -1;
                 for (var i = 0, len = aliens.length; i < len; i++) {
                     aliens[i].x += 30 * dir;
@@ -389,14 +400,15 @@ var GameState = State.extend({
                 }
             }
 
+            // Si il n'y a plus d'aliens
             if (aliens.length == 0 ){
 
+                //Recreer un tableau d'alien et augmente le niveau
                 var rows = [1, 0, 0, 2, 2];
                 for (var i = 0, len = rows.length; i < len; i++) {
                     for (var j = 0; j < 10; j++) {
                         var a = rows[i];
-                        // create right offseted alien and push to alien
-                        // array
+
                         aliens.push({
                             sprite: alSprite[a],
                             x: 30 + j*30 + [0, 4, 0][a],
@@ -415,39 +427,44 @@ var GameState = State.extend({
     },
 
 
-    /**
-     * Render the game state to the canvas
-     */
+   // efface et remet le canevas completement
     render: function (contx) {
 
+        //efface tout le canevas
+        contx.clearAll();
 
-        contx.clearAll();// clear the game canvas
-        // draw all aliens
+        // remet tous les aliens
         for (var i = 0, len = aliens.length; i < len; i++) {
             var a = aliens[i];
             contx.drawSprite(a.sprite[spFrame], a.x, a.y);
         }
 
+        // remet le score
         contx.vectorText(score.toString(), 3, 35, 10);
         for (var i = 0; i < lives; i++) {
             contx.drawSprite(tank.sprite, 400+25*i, 10);
         }
-        // save context and draw bullet then restore
+
+        // enregistre le context des balles et les redessine
         contx.save();
         for (var i = 0, len = bullets.length; i < len; i++) {
             contx.drawBullet(bullets[i]);
         }
+
+        //restaure le context
         contx.restore();
 
-        // draw the city graphics to the canvas
+        // remet les villes
         contx.drawImage(cities.canvas, 0, cities.y);
-        // draw the tank sprite
+        //  remet le tank
         contx.drawSprite(tank.sprite, tank.x, tank.y);
 
+        // Efface le sprite du tank si il n'est pas visible
         if (tank.visible == false )
             contx.clearRect(tank.x,tank.y, taSprite.w, taSprite.h);
 
-        // draw game over messege
+        // si gameover
+        // met un message pour l'utilisateur
         if (gameOver) {
 
             contx.vectorText("Game Over", 4, null, null);
@@ -460,26 +477,21 @@ var GameState = State.extend({
 
 });
 
-
-
-
-
+// permet d'avoir plusieurs sons en memoire
 function SoundPool(maxSize) {
-    var size = maxSize; // Max sounds allowed in the pool
+    var size = maxSize;
     var pool = [];
     this.pool = pool;
     var currSound = 0;
-    /*
-     * Populates the pool array with the given sound
-     */
+
+
+    // permet d'attribuer une fonction a un son
     this.init = function(object) {
         switch(object){
 
-
-
+            //son shoot
             case "shoot" : {
                 for (var i = 0; i < size; i++) {
-                    // Initalize the sound
                     bullet = new Audio("audio/shoot.wav");
                     bullet.volume = .12;
                     bullet.load();
@@ -488,6 +500,7 @@ function SoundPool(maxSize) {
                 break;
             }
 
+            //son explosion
             case "explosion": {
                 for (var i = 0; i < size; i++) {
                     var explosion = new Audio("audio/explosion.wav");
@@ -498,6 +511,7 @@ function SoundPool(maxSize) {
                 break;
             }
 
+            //son invalide
             case "invaderKilled":
             {
                 for (var i = 0; i < size; i++) {
@@ -512,9 +526,8 @@ function SoundPool(maxSize) {
 
 
     };
-    /*
-     * Plays a sound
-     */
+
+    //lance les son 
     this.get = function() {
         if(pool[currSound].currentTime == 0 || pool[currSound].ended) {
             pool[currSound].play();
