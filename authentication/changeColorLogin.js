11da0ddeca4c1d692b2b944edd9ed63544d3ade8
Julien <julien.reichenbach@students.hevs.ch>
2018-06-05 19:25:41 +0200

    Login & logout pages takes care of background color

---
+++ authentication/changeColorLogin.js
@@ -0,0 +1,20 @@
+function changeColor(a){
+    var newColor = a.value;
+    localStorage.setItem("color", newColor);
+    changeColorTheme();
+}
+
+function changeColorTheme(){
+    if (localStorage.getItem("color") == null){
+        localStorage.setItem("color", "authentication/loginBlue.css");
+    }
+
+    var cssString = localStorage.getItem("color");
+    cssString = cssString.replace("background", "login");
+    cssString = cssString.replace("styles", "authentication");
+    alert(cssString);
+
+    var cssLink = document.getElementById("colorCSS");
+    cssLink.setAttribute("href", cssString);
+    window.reload();
+}
